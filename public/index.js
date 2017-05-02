5d8802ded5fa60a8c21a4e806d1b6c2615f47b80
Tyler Davis <tyjdavis@outlook.com>
2017-05-02 13:02:31 -0400

    chat box sends to firebase

--- public/index.js
+++ public/index.js
@@ -1,6 +1,22 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDNG_Quc7OiSfplBGjcYDA7B37GRZ13gZY",
+  authDomain: "user-auth-25bd3.firebaseapp.com",
+  databaseURL: "https://user-auth-25bd3.firebaseio.com",
+  projectId: "user-auth-25bd3",
+  storageBucket: "user-auth-25bd3.appspot.com",
+  messagingSenderId: "829467504471"
+};
+firebase.initializeApp(config);
+
+let database = firebase.database();
+let data = firebase.database().ref('data');
+
+
+//Logging Existing User in
+
 let logInButton = document.getElementById('logIn');
 logInButton.addEventListener('click', logInUser)
-
 
 function logInUser () {
 
@@ -17,30 +33,37 @@ function logInUser () {
     var errorCode = error.code;
     var errorMessage = error.message;
   });
-    JWT();
+    let messageButton = document.getElementById('message-button');
+    messageButton.addEventListener('click', message);
+
+    function message () {
+      let messageText = document.getElementById('message').value;
+
+      firebase.database().ref('data/').push({
+        email: email,
+        text: messageText,
+      });
+    }
+  JWT();
 }
 
-//prints the JWT of the user to the console
 function JWT () {
   firebase.auth().currentUser.getToken(true).then(function(idToken) {
     console.log(idToken);
     }).catch(function(error) {
-      // Handle error
     });
 }
 
 
-//Create User
+//Creating a New User
 
 let signUpButton = document.getElementById('signUp');
 signUpButton.addEventListener('click', signUpUser)
-
 
 function signUpUser () {
 
   let email = document.querySelector('#signup-email').value;
   let password = document.querySelector('#signup-password').value;
-  //let errorDiv = document.querySelector('.signup-error');
   console.log("Email:", email, "Password:", password);
 
 
@@ -52,37 +75,19 @@ function signUpUser () {
     }
   })
   .catch(function(error) {
-    // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
-    //errorDiv.textContent = errorMessage;
     console.log(errorMessage);
   });
+  let messageButton = document.getElementById('message-button');
+  messageButton.addEventListener('click', message);
+
+  function message () {
+    let messageText = document.getElementById('message').value;
+
+    firebase.database().ref('data/').push({
+      email: email,
+      text: messageText,
+    });
+  }
 }
-
-
-
-//messaging
-
-let messageButton = document.getElementById('message-button');
-messageButton.addEventListener('click', message);
-
-function message () {
-  let messageText = document.querySelector('#message').value;
-  console.log(messageText);
-}
-
-
-
-
-
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyDNG_Quc7OiSfplBGjcYDA7B37GRZ13gZY",
-  authDomain: "user-auth-25bd3.firebaseapp.com",
-  databaseURL: "https://user-auth-25bd3.firebaseio.com",
-  projectId: "user-auth-25bd3",
-  storageBucket: "user-auth-25bd3.appspot.com",
-  messagingSenderId: "829467504471"
-};
-firebase.initializeApp(config);
